21c2b960d000cf31b20b5ed09c4c722ea06e5463
idealtitude <idealtitude@gmail.com>
2015-07-12 13:19:45 +0200

    Test levels.blend, utilisation de libload + barre de pogression, nombreux messages d'erreur côté blend chargé (map_test_2.blend)

--- levels.py
+++ levels.py
@@ -2,7 +2,7 @@ import sys
 import os
 import aud
 
-import subprocess as sp
+import threading
 
 # So we can find the bgui module
 sys.path.append('../..')
@@ -35,30 +35,34 @@ class MainMenu(bgui.bge_utils.Layout):
 
         # A themed frame
         self.win = bgui.Frame(self, size=[0.6, 0.8], pos=[0.01, 0.01], options=bgui.BGUI_DEFAULT)
-        
+
         self.check_loaded = False
-        
+
         blendfile = TRACKS_PATH + 'map_test_2.blend'
-        
+
         if 'current_level' in bge.logic.globalDict:
             blendfile = bge.logic.globalDict['current_level']
-            
-        handle = bge.logic.LibLoad(blendfile, 'Scene', async=True)
-        handle.onFinish = self.level_loaded
-        
+
+        self.handle = bge.logic.LibLoad(blendfile, 'Scene', async=True)
+        self.handle.onFinish = self.level_loaded
+
         self.progress = bgui.ProgressBar(self.win, percent=0.1, size=[0.92, 0.06], pos=[.2, 0.17], sub_theme="Progress", options=bgui.BGUI_DEFAULT | bgui.BGUI_CENTERX)
-        
-        #while self.check_loaded == False:
-         #   val = "%.2f" % handle.progress
-            #self.progress.percent = val
-            #print(val)
-    
+
+        progress_thread = threading.Thread(target=self.update_progress)
+        progress_thread.start()
+
+    def update_progress(self):
+        while self.check_loaded == False:
+            val = "%.2f" % self.handle.progress
+            self.progress.percent = eval(val)
+            print(val)
+
     def level_loaded(self, e):
         self.check_loaded = True
         loader = bge.logic.getCurrentScene()
         loader.end()
         print('Loaded !')
-        
+
 def main(cont):
     own = cont.owner
     mouse = bge.logic.mouse
